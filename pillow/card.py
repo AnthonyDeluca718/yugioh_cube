48198f4d0e90244c2cc611c38def8760742de330
Anthony <anthony@expandtheroom.com>
2019-04-20 20:48:46 -0400

    Accel Synchron looks good

--- pillow/card.py
+++ pillow/card.py
@@ -2,9 +2,12 @@ from PIL import Image, ImageDraw, ImageFont
 import os, json, textwrap
 
 fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 12)
+fnt_black = ImageFont.truetype('/Library/Fonts/Arial Black.ttf', 14)
 char_limit = 60
 ascent, descent = fnt.getmetrics()
 line_height = ascent + descent + 1
+ascent_black, descent_black = fnt_black.getmetrics()
+line_height_black = ascent_black + descent_black + 1
 
 def add_text(text, stats, types, image, output_name):
 
@@ -16,14 +19,15 @@ def add_text(text, stats, types, image, output_name):
     text_content = " ".join(text_lines)
     lines = textwrap.wrap(text_content, char_limit)
 
-  lines = [types] + lines + [stats]
   width, height = image.size
-  box_height = (len(lines))*(ascent + descent + 2)
+  box_height = (len(lines))*line_height + 2 * line_height_black
   text_box = "\n".join(lines)
 
   draw = ImageDraw.Draw(image)
   draw.rectangle([(0, height - box_height), (width, height)], (255, 255, 255))
-  draw.text((3, height-box_height), text_box, font=fnt, fill=(0, 0, 0))
+  draw.text((3, height - box_height), types, font=fnt_black, fill=(0, 0, 0))
+  draw.text((3, height - box_height + line_height_black), text_box, font=fnt, fill=(0, 0, 0))
+  draw.text((3, height - line_height_black), stats, font=fnt_black, fill=(0, 0, 0))
 
   image.save(output_name)
 
