e9f05ae8440419540f7135fc10e64ef18ce91d1b
Anthony <anthony@expandtheroom.com>
2019-04-22 14:06:06 -0400

    promist.all cardlist

--- pillow/createJSON.js
+++ pillow/createJSON.js
@@ -1,18 +1,26 @@
 const getData = require('./getData')
-
 const fs = require('fs')
+
 // const LineByLine = require('line-by-line')
 
 // getData('Judgement Dragon', 'test')
 
-const createJSON = (file, folder) => {
+const createJSON = (file, folder, name) => {
   if (!fs.existsSync(`./${folder}`)) {
     fs.mkdirSync(`./${folder}`)
   }
 
+  const cards = {}
+
   const lines = fs.readFileSync(file, 'utf-8').split('\n').filter(Boolean)
 
-  console.log(lines)
+  const promises = lines.map(line => {
+    return getData(line)
+      .then(data => cards[line] = data)
+      .catch(() => console.log(error))
+  });
+
+  Promise.all(promises).then(() => fs.writeFileSync(`./${folder}/${name}.json`, JSON.stringify(cards, null, 4)))
 }
 
-createJSON('testList', 'test')
+createJSON('testList', 'test', 'cardlist')
