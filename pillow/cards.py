3b3385c57599dbf0474122131ada0c6b4c18bb23
Anthony <anthony@expandtheroom.com>
2019-04-24 14:08:58 -0400

    most card added

--- pillow/cards.py
+++ pillow/cards.py
@@ -5,5 +5,9 @@ path = os.path.join(os.path.dirname(__file__), 'cardlist.json')
 data_file = open(path, 'r')
 data = json.load(data_file)
 
-for name in data:
+start = 342
+keys = list(data.keys())
+for i in range(start, len(keys)):
+  print(i)
+  name = keys[i]
   save_image(name, data[name], 'images', 'image-output')
